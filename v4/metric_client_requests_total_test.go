d22473f1fb51c9519660ad3e29c49fdfbab571b2
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2020-11-11 17:24:03 +0100

    v4: TestNewClientRequestsTotalStatsHandler

---
+++ v4/metric_client_requests_total_test.go
@@ -0,0 +1,50 @@
+package promgrpc_test
+
+import (
+	"context"
+	"strings"
+	"testing"
+	"time"
+
+	"google.golang.org/grpc/metadata"
+
+	"github.com/piotrkowalczuk/promgrpc/v4"
+	"github.com/prometheus/client_golang/prometheus/testutil"
+	"google.golang.org/grpc/stats"
+)
+
+func TestNewClientRequestsTotalStatsHandler(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	h := promgrpc.NewStatsHandler(promgrpc.NewClientRequestsTotalStatsHandler(promgrpc.NewClientRequestsTotalCounterVec()))
+	ctx = h.TagRPC(ctx, &stats.RPCTagInfo{
+		FullMethodName: "/service/Method",
+		FailFast:       true,
+	})
+	h.HandleRPC(ctx, &stats.OutHeader{
+		Client: true,
+		Header: metadata.MD{"user-agent": []string{"fake-user-agent"}},
+	})
+	h.HandleRPC(ctx, &stats.OutHeader{
+		Client: true,
+	})
+	h.HandleRPC(ctx, &stats.OutHeader{
+		Client: true,
+	})
+	h.HandleRPC(ctx, &stats.OutHeader{
+		Client: false,
+	})
+
+	const metadata = `
+		# HELP grpc_client_requests_sent_total TODO
+        # TYPE grpc_client_requests_sent_total counter
+	`
+	expected := `
+		grpc_client_requests_sent_total{grpc_client_user_agent="fake-user-agent",grpc_is_fail_fast="true",grpc_method="Method",grpc_service="service"} 3
+	`
+
+	if err := testutil.CollectAndCompare(h, strings.NewReader(metadata+expected), "grpc_client_requests_sent_total"); err != nil {
+		t.Fatal(err)
+	}
+}
