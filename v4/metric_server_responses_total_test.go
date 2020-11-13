2d64bd207ac0796373f7f5cb373016bb4a254a9a
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2020-11-13 12:22:31 +0100

    v4: TestNewServerResponsesTotalStatsHandler

---
+++ v4/metric_server_responses_total_test.go
@@ -0,0 +1,47 @@
+package promgrpc_test
+
+import (
+	"context"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/piotrkowalczuk/promgrpc/v4"
+	"github.com/prometheus/client_golang/prometheus/testutil"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/stats"
+	"google.golang.org/grpc/status"
+)
+
+func TestNewServerResponsesTotalStatsHandler(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	h := promgrpc.NewStatsHandler(promgrpc.NewServerResponsesTotalStatsHandler(promgrpc.NewServerResponsesTotalCounterVec()))
+	ctx = metadata.NewIncomingContext(ctx, metadata.MD{"user-agent": []string{"fake-user-agent"}})
+	ctx = h.TagRPC(ctx, &stats.RPCTagInfo{
+		FullMethodName: "/service/Method",
+		FailFast:       true,
+	})
+	h.HandleRPC(ctx, &stats.End{
+		Error: status.Error(codes.Aborted, "aborted"),
+	})
+	h.HandleRPC(ctx, &stats.End{})
+	h.HandleRPC(ctx, &stats.End{
+		Client: true,
+	})
+
+	const metadata = `
+		# HELP grpc_server_responses_sent_total TODO
+        # TYPE grpc_server_responses_sent_total counter
+	`
+	expected := `
+		grpc_server_responses_sent_total{grpc_client_user_agent="fake-user-agent",grpc_code="Aborted",grpc_method="Method",grpc_service="service"} 1
+        grpc_server_responses_sent_total{grpc_client_user_agent="fake-user-agent",grpc_code="OK",grpc_method="Method",grpc_service="service"} 1
+	`
+
+	if err := testutil.CollectAndCompare(h, strings.NewReader(metadata+expected), "grpc_server_responses_sent_total"); err != nil {
+		t.Fatal(err)
+	}
+}
